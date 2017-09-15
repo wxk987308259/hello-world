a742cbad05d9d2ebdd8189923f2dabba9d7a8e50
wangxk <wangxk@wangxk-X71.(none)>
2017-09-15 19:29:29 +0800

    test love
    Signed-off-by: wangxk <wangxk@wangxk-X71.(none)>

--- src/main.c
+++ src/main.c
@@ -2,15 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+//#include <stdbool.h>
+#include <assert.h>
 
-int main()
+int main(int argc,char *argv[])
 {
-	printf("Only a test!\n");
+	if(argc!=3)
+	{
+		printf("Usage:./xxx money(0~5) handsome(0~5)\n");
+		return -1;
+	}
+	
+	int years=0;
+	int money=atoi(argv[1]);
+	int handsome=atoi(argv[2]);
+	
+	assert(money<=5&&money>=0);
+	assert(handsome<=5&&money>=0);
+	assert((handsome+money)<=10&&(handsome+money)>=0);
 	
 	while(1)
 	{
-		printf("I Love You!\n");
+		if((money+handsome)<5)
+		{
+			printf("We will never fell in love!\n");
+			break;
+		}
+		
+		years++;
+		
+		if(years>=7)
+		{
+			printf("I don't love you any more!\n");
+			break;
+		}
+		
+		printf("I love you!\n");
+		
+		sleep(1);
 	}
+	
+	printf("Bye!..\n");
 
 	return 0;
 }
